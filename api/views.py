c87a7457df085cf23f4aaa5ca95a402ea4e5bc56
Soumen <soumensaha@gmail.com>
2022-06-17 12:47:39 +0530

    Integrate Model & View

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from base.models import Project
+from .serializers import ProjectSerializer
 
 @api_view(['GET'])
 def get_data(request):
-    project = {'id':'dummyProjectId'}
-    return Response(project)
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
